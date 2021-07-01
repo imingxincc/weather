28dfc4aec8c4abadf4fd79682f9d9f90659ed4ef
77tehui <77tehui@example.com>
2021-07-01 17:48:07 +0800

    编写单元测试

---
+++ src/Exceptions/InvalidArgumentException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Imingxin\Weather\Exceptions;
+
+class InvalidArgumentException extends Exception
+{
+
+}
---
+++ src/Weather.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Imingxin\Weather;
+
+use GuzzleHttp\Client;
+use Imingxin\Weather\Exceptions\HttpException;
+use Imingxin\Weather\Exceptions\InvalidArgumentException;
+
+class Weather
+{
+    protected $key;
+    protected $guzzleOptions = [];
+
+    public function __construct($key)
+    {
+        $this->key = $key;
+    }
+
+    public function getHttpClient()
+    {
+        return new Client($this->guzzleOptions);
+    }
+
+    public function setGuzzleOptions(array $options)
+    {
+        $this->guzzleOptions = $options;
+    }
+
+    public function getWeather($city, string $type = 'base', string $format = 'json')
+    {
+        if (!\in_array(\strtolower($format), ['json', 'xml'])) {
+            throw new InvalidArgumentException('Invalid response format: ' . $format);
+        }
+
+        if (!\in_array(\strtolower($type), ['base', 'all'])) {
+            throw new InvalidArgumentException('Invalid type value(base/all): ' . $type);
+        }
+
+        $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
+
+        $query = array_filter([
+            'key' => $this->key,
+            'city' => $city,
+            'extensions' => $type,
+            'output' => $format,
+        ]);
+
+        try {
+            $response = $this->getHttpClient()->get($url, [
+                'query' => $query,
+            ])->getBody()->getContents();
+            return 'json' === $format ? \json_decode($response, true) : $response;
+        } catch (\Exception $e) {
+            throw new HttpException($e->getMessage(), $e->getCode(), $e);
+        }
+    }
+}
